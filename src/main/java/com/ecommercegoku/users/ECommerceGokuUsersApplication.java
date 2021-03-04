b3523b8c1dc2d2bbfe338a4849f845adfc048db0
Rafhael de Andrade Barabas <rafhael_ab@hotmail.com>
2021-03-04 12:46:54 -0300

    initial commit

---
+++ src/main/java/com/ecommercegoku/users/ECommerceGokuUsersApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercegoku.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceGokuUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceGokuUsersApplication.class, args);
+	}
+
+}
